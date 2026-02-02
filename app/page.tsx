089d0e75cc27096fe289b41b1e1251ebd527ce67
Akil <akhilal778@gmail.com>
2026-02-02 07:31:24 +0530

    Force Dynamic Rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Hero from "@/components/HeroSection";
 import { prisma } from "@/lib/prisma";
 
+export const dynamic = "force-dynamic"; 
+
 export default async function Home() {
   let backgroundImage = "";
   try {
